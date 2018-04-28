de8fafdeb631c6f0cf808866f34f4b514ee64f28
georgezeng <zzeng@bglcorp.com.au>
2018-04-28 17:10:10 +0800

    no message

--- test.gateway/src/main/java/net/geozen/cloud/gateway/config/Config.java
+++ test.gateway/src/main/java/net/geozen/cloud/gateway/config/Config.java
@@ -1,19 +1,24 @@
 package net.geozen.cloud.gateway.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 public class Config {
+	@Autowired
+	private RefreshedConfig config;
 
 	@Bean
 	public LettuceConnectionFactory connectionFactory() {
-		return new LettuceConnectionFactory();
+		LettuceConnectionFactory factory = new LettuceConnectionFactory();
+		factory.setHostName(config.getRedisHost());
+		return factory;
 	}
 
-//	@Bean
-//	public HeaderHttpSessionStrategy sessionStrategy() {
-//		return new HeaderHttpSessionStrategy();
-//	}
+	// @Bean
+	// public HeaderHttpSessionStrategy sessionStrategy() {
+	// return new HeaderHttpSessionStrategy();
+	// }
 }
---
+++ test.gateway/src/main/java/net/geozen/cloud/gateway/config/RefreshedConfig.java
@@ -0,0 +1,21 @@
+package net.geozen.cloud.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@RefreshScope
+@Component
+public class RefreshedConfig {
+	@Value("${spring.redis.host}")
+	private String redisHost;
+
+	public String getRedisHost() {
+		return redisHost;
+	}
+
+	public void setRedisHost(String redisHost) {
+		this.redisHost = redisHost;
+	}
+
+}
